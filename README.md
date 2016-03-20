885e28c88613a3639857ff11cd5f03ed5939794a
tketron <tketron@gmail.com>
2016-03-19 19:28:14 -0700

    Create grid on page load

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # gridpad
+jQuery project from The Odin Project. http://www.theodinproject.com/web-development-101/javascript-and-jquery?ref=lnav
---
+++ javascript.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	var nsquares = 16;
+	var $container = $('#container');
+	//get css for container div
+	var containerWidth = $($container).width();
+	var containerHeight = $($container).height();
+	//set css for grid-square
+	var gridSquareWidth = containerWidth / nsquares;
+	var gridSquareHeight = containerHeight / nsquares;
+	for (n=0; n<(nsquares*nsquares); n++) {
+		$($container).append('<div class="grid-square"></div>');
+		$($container).find('div').height(gridSquareHeight);
+		$($container).find('div').width(gridSquareWidth);
+	};
+});
