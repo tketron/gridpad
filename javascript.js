cef51e277d863f4e7a41d2079da6077c44778365
tketron <tketron@gmail.com>
2016-03-20 22:22:26 -0700

    Squares change to random color on hover

--- javascript.js
+++ javascript.js
@@ -12,4 +12,9 @@ $(document).ready(function() {
 	};
 	$($container).find('div').height(gridSquareHeight);
 	$($container).find('div').width(gridSquareWidth);
+
+	//grid hover effect
+	$(".grid-square").mouseenter(function() {
+		$(this).css("background-color", "#"+Math.floor(Math.random()*16777215).toString(16));
+	});
 });
